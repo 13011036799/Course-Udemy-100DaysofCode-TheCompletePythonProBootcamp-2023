f33169e3f23eaffd26e2b23ab2ad4dc2cc03d44c
Chichi Zhang <93120868+13011036799@users.noreply.github.com>
2023-05-12 16:43:07 +0800

    Create project9_Blind-auctionGame.py

---
+++ project9_Blind-auctionGame.py
@@ -0,0 +1,25 @@
+from replit import clear
+from art import logo
+
+def find_highest_bidder(bids):
+    highest_bid = 0
+    winner = ""
+    for name in bids:
+        if highest_bid<bids[name]:
+            highest_bid=bids[name]
+            winner=name
+    print(f"The winner is {name}. The price is {highest_bid}.")
+
+print(logo+"\n")
+loop=True
+bids={}
+while loop:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue=="no":
+        loop = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
